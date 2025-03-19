d2c759b9a1b5a1aa148584fab101ec879938bb38
Ayush <ayushmishra7484@gmail.com>
2025-03-19 09:36:17 +0530

    First commit to to_do list

---
+++ toDoList/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+const todoInput=document.getElementById("todo-input")
+const addTaskButton=document.getElementById("add-task-btn")
+const todoList=document.getElementById("todo-list")
+
+let tasks=JSON.parse(localStorage.getItem("tasks")) || []; 
+
+tasks.forEach(task => renderTask(task))
+
+addTaskButton.addEventListener('click', ()=>{
+    const taskText=todoInput.value.trim()
+    if(taskText==="") return
+
+    const newTask={
+        id: Date.now(),
+        text: taskText,
+        completed: false
+    }
+    tasks.push(newTask)
+    saveTask();
+    todoInput.value=""; //clear input
+    console.log(tasks);
+    
+})
+
+function renderTask(task){
+    console.log(task);
+    
+}
+function saveTask(){
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+// todoInput.addEventListener("click", ()=>{
+//     newTask.completed=true
+// })
+})
